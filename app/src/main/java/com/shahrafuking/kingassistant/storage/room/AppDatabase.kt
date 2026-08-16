b4f9796d7335279a1eb89c4429a7508e8b5d1e58
AKAlif08 <alifislsm444@gmail.com>
2026-08-16 20:14:24 +0600

    Disable exportSchema in AppDatabase annotation

--- app/src/main/java/com/shahrafuking/kingassistant/storage/room/AppDatabase.kt
+++ app/src/main/java/com/shahrafuking/kingassistant/storage/room/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [MemoryEntity::class, PendingActionEntity::class, VoiceProfileEntity::class], version = 1)
+@Database(entities = [MemoryEntity::class, PendingActionEntity::class, VoiceProfileEntity::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun memoryDao(): MemoryDao
     abstract fun pendingActionDao(): PendingActionDao
