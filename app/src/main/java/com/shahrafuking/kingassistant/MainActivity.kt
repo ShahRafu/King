583287cf023286da79aecd625289e83f0f98336e
MLSabbir <sabbirml721@gmail.com>
2026-08-16 06:45:50 +0600

    Refactor MainActivity to use AppRoot composable

--- app/src/main/java/com/shahrafuking/kingassistant/MainActivity.kt
+++ app/src/main/java/com/shahrafuking/kingassistant/MainActivity.kt
@@ -3,28 +3,32 @@ package com.shahrafuking.kingassistant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                        Text(text = "King Assistant is Running!")
-                    }
-                }
-            }
+            AppRoot()
         }
     }
+}
+
+@Composable
+fun AppRoot() {
+    MaterialTheme {
+        Surface {
+            Text(text = "Welcome to King Assistant")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAppRoot() {
+    AppRoot()
 }
